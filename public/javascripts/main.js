d2deca1ca6f372b16f851f610a594dda41a9cabf
abhishek.ch <abhishek.ch@directi.com>
2017-11-13 19:25:03 +0530

    Dojo first commit

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,46 @@
-require(['javascripts/reddit.js'], function (reddit) {
-   
-    new reddit.reddit();
-    
+// require(['javascripts/reddit.js'], function (reddit) {
+//
+//    //new reddit.reddit();
+//
+// });
+
+define([
+    "dojo/_base/declare",
+    "dijit/_WidgetBase",
+    "dijit/_Templated"/*,
+    "dojo/domReady!"*/
+], function (declare, _WidgetBase, _Templated) {
+    alert("hmm");
+
+    // var widgetClass = declare([_WidgetBase, _Templated], {
+    //     templateString: buttonTemplate,
+    //     onClicked: function () {
+    //         var widget = new MyScript();
+    //         widget.placeAt(this.bottomBlock);
+    //     }
+    // });
+
+    // new widgetClass().placeAt(document.body);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
